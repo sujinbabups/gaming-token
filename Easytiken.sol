0a93a976870831a598acb776957439f8fa221174
sujinbabups <sujinbu85@gmail.com>
2024-09-09 00:06:36 +0530

    token

---
+++ Easytiken.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract EasyToken is ERC20{
+    address public   admin;
+
+    constructor()ERC20("Easy Token","ET") {
+        _mint(msg.sender, 200000000000000000000);
+        admin=msg.sender;
+                
+    }
+     modifier onlyAdmin{
+        require(msg.sender==admin,"Unautherized");
+        _;
+    }
+    function safeMint(address to,uint value) public onlyAdmin{
+        _mint(to, value);
+    }
+}
