0245aac20cb23df33ea3a430b028266e06724661
sujinbabups <sujinbu85@gmail.com>
2024-09-09 15:22:46 +0530

    updated

--- Easytoken.sol
+++ Easytoken.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+
+import "ERC20.sol";
 
 contract EasyToken is ERC20{
     address public   admin;
